963e99c95245e82bafd94de6db20c244268a0586
Tiffany Huoth <Tiffany@Tiffany-PC.stanford.edu>
2012-10-17 14:41:50 -0700

    ensuring labwork is in personal folder

---
+++ week3/config.php
@@ -0,0 +1,9 @@
+<?php
+<<<<<<< HEAD
+$link = mysql_connect('mysql-user-master.stanford.edu', 'ccs147thuoth', 'eejaecoh');
+mysql_select_db('c_cs147_thuoth');
+=======
+$link = mysql_connect('mysql-user-master.stanford.edu', 'ccs147rakasaka', 'ofairahf');
+mysql_select_db('c_cs147_rakasaka');
+>>>>>>> b3dacea09ff430bf4988b594f3098ed8216df35c
+?>
